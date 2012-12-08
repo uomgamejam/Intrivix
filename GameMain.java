3d300b89f04c17904d0486da8bd3fd52374bcb0e
Devin Ridgway <devin.ridgway@gmail.com>
2012-12-08 16:37:34 +0000

    Initial game code commit

---
+++ GameMain.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Intrivix;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+
+/**
+ *
+ * @author Devin
+ */
+public class GameMain extends BasicGame{    
+    
+    TrueTypeFont ttFont;
+    
+    public GameMain(){
+        super("Game Jam");
+    }
+    
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        java.awt.Font f = new java.awt.Font("Consolas", java.awt.Font.PLAIN, 15);
+        ttFont = new TrueTypeFont(f, true);
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
