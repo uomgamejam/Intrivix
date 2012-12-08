5ca821e15465ae82a253e86fee9091dd6b1d3766
George Vanburgh <george.vanburgh@cs.manchester.ac.uk>
2012-12-08 18:52:59 +0000

    Basic levelloader

--- game/LevelLoader.java
+++ game/LevelLoader.java
@@ -12,6 +12,13 @@ import java.util.Scanner;
 import java.io.*;
 
 public class LevelLoader {
+    //String to contain the location of the level files
     private final static String LEVEL_DIR = "assets/levels/";
     
+    public LevelLoader(String levelname)
+    {
+        String levelPath = LEVEL_DIR + levelname;
+        
+    }
 }
+
