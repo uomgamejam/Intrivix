1c4d123c6f007b103aaf98d816b62e2664efb2ca
George Vanburgh <george.vanburgh@cs.manchester.ac.uk>
2012-12-08 18:45:37 +0000

    Created LevelLoader class

---
+++ game/LevelLoader.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Intrivix.game;
+
+/**
+ *
+ * @author George
+ */
+import java.util.Scanner;
+import java.io.*;
+
+public class LevelLoader {
+    private final static String LEVEL_DIR = "assets/levels/";
+    
+}
