23388b1e240d7b5ed62ddb8f295363969052f0a6
eNcoRe101 <cricket-rocks@hotmail.co.uk>
2012-12-08 18:31:49 +0000

    add background class

---
+++ game/BackgroundImage.java
@@ -0,0 +1,22 @@
+/*
+ * class to create a background image for a JPane
+ */
+package game;
+
+import java.awt.Image;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rich
+ */
+public class BackgroundImage extends JPanel
+{
+    
+    private Image backgroudImage;
+    
+    public BackgroundImage()
+    {
+        
+    }
+}
