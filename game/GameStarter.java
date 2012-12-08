441b038f16b480696d9cb7cbd83e48817c3956ff
eNcoRe101 <cricket-rocks@hotmail.co.uk>
2012-12-08 23:26:12 +0000

    Fixed RICHARDS shit@

--- game/GameStarter.java
+++ game/GameStarter.java
@@ -3,17 +3,32 @@
  * and open the template in the editor.
  */
 package Intrivix.game;
+
+import java.awt.Container;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Devin
  */
-import game.GameMenu;
-public class GameStarter {
+
+public class GameStarter extends JFrame {
+    
+    GameMenu menu;
     
     GameStarter(){
         //Here is where we start the regular menu.
         //this class is simply here to start all the things
-        GameMenu gm = new GameMenu();
+        
+        super("$Game");
+        setBounds(50, 0, GameMain.SMALL_SCREEN_WIDTH, GameMain.SMALL_SCREEN_HEIGHT);
+        setResizable(false);
+        ExitWindow exit = new ExitWindow();
+        addWindowListener(exit);
+        
+        menu = new GameMenu(this);
+        setContentPane(menu);
+        setVisible(true);
     }
     
     public static void main(String[] args){
