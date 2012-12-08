fff1813410e89132c7710b806f8e33454787f950
George Vanburgh <george.vanburgh@cs.manchester.ac.uk>
2012-12-08 20:46:11 +0000

    Working LevelLoader
    Still needs object creation methods

--- game/LevelLoader.java
+++ game/LevelLoader.java
@@ -10,15 +10,50 @@ package Intrivix.game;
  */
 import java.util.Scanner;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LevelLoader {
     //String to contain the location of the level files
     private final static String LEVEL_DIR = "assets/levels/";
     
-    public LevelLoader(String levelname)
+    public static void LevelLoader(String levelname)
     {
-        String levelPath = LEVEL_DIR + levelname;
-        
+        Scanner scan = null;
+        try {
+            String levelPath;
+            levelPath = LEVEL_DIR + levelname;
+            scan = new Scanner(new File(levelPath));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LevelLoader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        while (scan.hasNextLine())
+        {
+            String [] row = scan.nextLine().split("\\s+");
+            String objectType = row[4];
+            switch(objectType)
+            {
+                case "ground":
+                    System.out.println("I shall create a ground object");
+                    break;
+                case "background":
+                    System.out.println("I shall create a background object");
+                    break;
+                case "scrolling-background":
+                    System.out.println("I shall create a scrolling background object");
+                    break;
+                case "player-start":
+                    System.out.println("I shall create a player start object");
+                    break;
+                default:
+                    System.err.println("Level file: Undefined object");
+            }
+            System.out.println(row[4]);
+        }
+    }
+    public static void main(String [] args)
+    {
+        LevelLoader("level1.txt");
     }
 }
 
