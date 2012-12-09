54946d6becaaefd3c683c44d4bc97c314127d80c
Devin Ridgway <devin.ridgway@gmail.com>
2012-12-09 08:15:18 +0000

    full screen.. buggy as fuck... no longer give any fucks...

--- game/GameMain.java
+++ game/GameMain.java
@@ -4,6 +4,8 @@
  */
 package Intrivix.game;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import javax.swing.JFrame;
 
 /**
@@ -20,9 +22,10 @@ public class GameMain extends JFrame{
     Player player;
     
     GamePanel panel;
+    private GraphicsDevice gd;
     
-    public GameMain(){
-        super("$Game");
+    public GameMain(boolean fullScreen){
+        /*super("$Game");
         setBounds(50, 0, SMALL_SCREEN_WIDTH, SMALL_SCREEN_HEIGHT);
         setResizable(false);
         ExitWindow exit = new ExitWindow();
@@ -30,11 +33,44 @@ public class GameMain extends JFrame{
         
         panel = new GamePanel(this);
         setContentPane(panel);
+        setVisible(true);*/
+        super("$Game");
+        
+        if(fullScreen)
+        {
+            initFullScreen();
+        }
+        else
+        {
+            setBounds(50, 0, GameMain.SMALL_SCREEN_WIDTH, GameMain.SMALL_SCREEN_HEIGHT);
+        }
+        
+        setResizable(false);
+        ExitWindow exit = new ExitWindow();
+        addWindowListener(exit);
+        
+        panel = new GamePanel(this, fullScreen);
+        setContentPane(panel);
         setVisible(true);
     }
     
+    private void initFullScreen() {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        gd = ge.getDefaultScreenDevice();
+
+        setUndecorated(true);    // no menu bar, borders, etc. or Swing components
+        setIgnoreRepaint(true);  // turn off all paint events since doing active rendering
+        setResizable(false);
+
+        if (!gd.isFullScreenSupported()) {
+            System.out.println("Full-screen exclusive mode not supported");
+            System.exit(0);
+        }
+        gd.setFullScreenWindow(this); // switch on full-screen exclusive mode
+    }
+    
     public static void main(String[] args){
-        GameMain game = new GameMain();
+        GameMain game = new GameMain(true);
     }
     
 }
--- game/GameMenu.java
+++ game/GameMenu.java
@@ -233,7 +233,7 @@ public class GameMenu extends JPanel implements Runnable, KeyListener
     {
         switch(currentButtonIndex)
         {
-            case 1: GameMain game = new GameMain();
+            case 1: GameMain game = new GameMain(fullscreen);
                     gameStarter.setVisible(false); //you can't see me!
                     running = false;
                     gameStarter.dispose();
