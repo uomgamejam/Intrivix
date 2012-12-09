f9efe5efd00d609da7ebee4fa6949a0766b7df0f
George Vanburgh <george.vanburgh@cs.manchester.ac.uk>
2012-12-09 04:04:40 +0000

    Started hiscored

--- game/Hiscores.java
+++ game/Hiscores.java
@@ -8,6 +8,27 @@ package Intrivix.game;
  *
  * @author georgevanburgh
  */
+
 public class Hiscores {
+
+    private final static String HISCORE_PATH = "./src/Intrivix/game/assets/hiscore";
     
+    /**
+     *
+     * @return
+     */
+    public static String[] buildScoreArray()
+    {
+        
+    }
+    
+    /**
+     * @param score The score to be tested
+     * @return isHiscore Returns true if the score given is large enough to be
+     * on the hiscore board
+     */
+    public static boolean isHiscore(int givenScore)
+    {
+        
+    }
 }
